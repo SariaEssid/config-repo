30f9b918acdc37c1c15ac4ac75b96aab0025c025
saria <saria.essid@esprit.tn>
2024-02-05 09:36:02 +0100

    commit3

--- src/main/java/com/javainuse/ConfigServerApplication.java
+++ src/main/java/com/javainuse/ConfigServerApplication.java
@@ -2,16 +2,18 @@ package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author ESSID Saria
  */
 
+@EnableConfigServer
 @SpringBootApplication
-public class FirstApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
